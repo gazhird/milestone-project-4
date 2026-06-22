fb836299d9506b2b43eaca1c544fe6f88696ce35
gazhird <gazhird@live.co.uk>
2026-06-22 14:35:02 +0100

    add edit / update test

--- listings/tests.py
+++ listings/tests.py
@@ -7,7 +7,7 @@ from .models import Listing, Bid
 User = get_user_model()
 
 
-class ListingTest(TestCase):
+class CreateListingTest(TestCase):
     def test_create_listing(self):
         # test new listing
         user = User.objects.create_user(username="test01", password="password")
@@ -31,5 +31,35 @@ class ListingTest(TestCase):
         )
         self.assertEqual(listing.make, "Ford")
         self.assertEqual(listing.model, "Fiesta")
+
+
+class EditListingTest(TestCase):
+    def test_create_listing(self):
+        # test edit listing
+        user = User.objects.create_user(username="test02", password="password")
+        listing = Listing.objects.create(
+            make="Ford",
+            model="Fiesta",
+            year=2026,
+            registration="TEST 002",
+            mileage=100000,
+            fuel="petrol",
+            transmission="manual",
+            condition="good",
+            colour="Red",
+            doors=5,
+            description="Test Description 002",
+            starting_price=1000,
+            current_price=1000, # edit current price test
+            ends_at=timezone.now() + timedelta(days=1),
+            seller=user,
+            status="active"
+        )
+        # Edit price
+        listing.current_price = 2000
+        listing.save()
+        
+        updated = Listing.objects.get(id=listing.id)
+        self.assertEqual(updated.current_price, 2000)
 
 
