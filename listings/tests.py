d1d9ec948b79ef2877c005b0910530269139c36e
gazhird <gazhird@live.co.uk>
2026-06-22 14:49:31 +0100

    add delete test

--- listings/tests.py
+++ listings/tests.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from datetime import timedelta
-from .models import Listing, Bid
+from .models import Listing
 
 User = get_user_model()
 
+# CRUD tests 
 
 class CreateListingTest(TestCase):
     def test_create_listing(self):
@@ -34,7 +35,7 @@ class CreateListingTest(TestCase):
 
 
 class EditListingTest(TestCase):
-    def test_create_listing(self):
+    def test_edit_listing(self):
         # test edit listing
         user = User.objects.create_user(username="test02", password="password")
         listing = Listing.objects.create(
@@ -50,7 +51,7 @@ class EditListingTest(TestCase):
             doors=5,
             description="Test Description 002",
             starting_price=1000,
-            current_price=1000, # edit current price test
+            current_price=1000, # update current price 
             ends_at=timezone.now() + timedelta(days=1),
             seller=user,
             status="active"
@@ -63,3 +64,29 @@ class EditListingTest(TestCase):
         self.assertEqual(updated.current_price, 2000)
 
 
+class DelListingTest(TestCase):
+    def test_del_listing(self):
+        # test delete listing
+        user = User.objects.create_user(username="test03", password="password")
+        listing = Listing.objects.create(
+            make="Ford",
+            model="Fiesta",
+            year=2026,
+            registration="TEST 003",
+            mileage=100000,
+            fuel="petrol",
+            transmission="manual",
+            condition="good",
+            colour="Red",
+            doors=5,
+            description="Test Description 003",
+            starting_price=1000,
+            current_price=1000, 
+            ends_at=timezone.now() + timedelta(days=1),
+            seller=user,
+            status="active"
+        )
+        listing_id = listing.id
+        listing.delete()
+        with self.assertRaises(Listing.DoesNotExist):
+            Listing.objects.get(id=listing_id)
